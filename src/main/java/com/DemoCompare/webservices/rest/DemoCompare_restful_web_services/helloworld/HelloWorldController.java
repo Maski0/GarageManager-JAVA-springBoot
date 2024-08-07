49538ebd3eff9ffcae09cd715bdcfb92b65cb112
maski <maskikarsrikar@gmail.com>
2024-08-07 19:50:33 +0530

    first commit

---
+++ src/main/java/com/DemoCompare/webservices/rest/DemoCompare_restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,32 @@
+package com.DemoCompare.webservices.rest.DemoCompare_restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+// REST API
+@RestController
+public class HelloWorldController {
+	
+	// /hello_world
+	// "Hello World"
+	@GetMapping(path = "/hello-world")
+	public String helloWorld() {
+		return "HelloWorld";
+	}
+	
+	// Json return
+	// java Bean..
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Fuck your mom");
+	}
+	
+	// Path Parameters
+	// /hello-world/path-variable/{name}
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name ) {
+		return new HelloWorldBean(String.format("Hello %s", name));
+	}
+}
---
+++ src/test/java/com/DemoCompare/webservices/rest/DemoCompare_restful_web_services/DemoCompareRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DemoCompare.webservices.rest.DemoCompare_restful_web_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCompareRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
