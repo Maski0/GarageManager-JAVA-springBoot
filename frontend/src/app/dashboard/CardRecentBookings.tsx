939407be5cb094aea23bfd9712b35fe31dea710c
maski <maskikarsrikar@gmail.com>
2024-09-01 17:21:57 +0530

    DashBoard layout And card for bookings.

---
+++ frontend/src/app/dashboard/CardRecentBookings.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CardRecentBookings = () => {
+    const isLoading = false;
+    return (
+        <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl pb-16"> 
+            {isLoading ? (
+                <div className="m-5">Loading...</div>
+            ) : (
+                <>
+                    <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
+                        Recent Bookings
+                    </h3>
+                </>
+            )} 
+        </div>
+    )
+}
+
+export default CardRecentBookings;
