798472fe4e68c9f9f4c5ffabcab576b53c37f431
maski <maskikarsrikar@gmail.com>
2024-09-03 00:56:00 +0530

    Revert "Sad Removing GetCustomDtos"
    This reverts commit a9c78746c9df8d4e5785e4f5904fdbb1f2d330f8.

--- backend/src/main/java/com/GarageManager/webservices/rest/GarageManager_restful_web_services/Employees/EmployeeCustomDTOs.java
+++ backend/src/main/java/com/GarageManager/webservices/rest/GarageManager_restful_web_services/Employees/EmployeeCustomDTOs.java
@@ -1,5 +1,96 @@
 package com.GarageManager.webservices.rest.GarageManager_restful_web_services.Employees;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.GarageManager.webservices.rest.GarageManager_restful_web_services.Bookings.BookingCustomDTOs.GetBookingDTO;
+
 public class EmployeeCustomDTOs {
 	
+	public static class GetEmployeeDTO {
+		
+		private Integer employeeId;
+	    private String employeeName;
+	    private String address;
+	    private String email;
+	    private Long phoneNumber;
+	    private LocalDate dateOfJoining;
+	    
+	    private List<GetBookingDTO> bookings;
+	    
+	    public static GetEmployeeDTO fromEntity(Employee employee) {
+	    	GetEmployeeDTO dto = new GetEmployeeDTO();
+	        dto.setEmployeeId(employee.getEmployee_id());
+	        dto.setEmployeeName(employee.getEmployee_name());
+	        dto.setAddress(employee.getAddress());
+	        dto.setEmail(employee.getE_mail());
+	        dto.setPhoneNumber(employee.getPhoneNumber());
+	        dto.setDateOfJoining(employee.getDate_of_Joining());
+	        
+	        dto.setBookings(employee.getBookings().stream()
+	        		.map(GetBookingDTO::fromEntity)
+	        		.collect(Collectors.toList()));
+	        
+	        return dto;
+	    }
+
+		public Integer getEmployeeId() {
+			return employeeId;
+		}
+
+		public void setEmployeeId(Integer employeeId) {
+			this.employeeId = employeeId;
+		}
+
+		public String getEmployeeName() {
+			return employeeName;
+		}
+
+		public void setEmployeeName(String employeeName) {
+			this.employeeName = employeeName;
+		}
+
+		public String getAddress() {
+			return address;
+		}
+
+		public void setAddress(String address) {
+			this.address = address;
+		}
+
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public Long getPhoneNumber() {
+			return phoneNumber;
+		}
+
+		public void setPhoneNumber(Long phoneNumber) {
+			this.phoneNumber = phoneNumber;
+		}
+
+		public LocalDate getDateOfJoining() {
+			return dateOfJoining;
+		}
+
+		public void setDateOfJoining(LocalDate dateOfJoining) {
+			this.dateOfJoining = dateOfJoining;
+		}
+
+		public List<GetBookingDTO> getBookings() {
+			return bookings;
+		}
+
+		public void setBookings(List<GetBookingDTO> bookings) {
+			this.bookings = bookings;
+		}
+		
+	}
+
 }
--- backend/src/main/java/com/GarageManager/webservices/rest/GarageManager_restful_web_services/Payments/PaymentCustomDTOs.java
+++ backend/src/main/java/com/GarageManager/webservices/rest/GarageManager_restful_web_services/Payments/PaymentCustomDTOs.java
@@ -1,6 +1,91 @@
 package com.GarageManager.webservices.rest.GarageManager_restful_web_services.Payments;
 
-public class PaymentCustomDTOs {
+import java.time.LocalDate;
 
+import com.GarageManager.webservices.rest.GarageManager_restful_web_services.Bookings.BookingCustomDTOs.GetBookingDTO;
+
+public class PaymentCustomDTOs {
+	
+	public static class GetPaymentDTO {
+		 private Integer paymentId;
+		 private LocalDate paymentDate;
+		 private Integer paymentAmount;
+		 private String paymentMethod;
+		 private String paymentStatus;
+		 private GetBookingDTO booking;
+		 
+		 
+		 public static GetPaymentDTO fromEntity(Payment payment) {
+			 	GetPaymentDTO dto = new GetPaymentDTO();
+		        dto.setPaymentId(payment.getPayment_id());
+		        dto.setPaymentDate(payment.getPaymentDate());
+		        dto.setPaymentAmount(payment.getPaymentAmount());
+		        dto.setPaymentMethod(payment.getPaymentMethod());
+		        dto.setPaymentStatus(payment.getPaymentStatus());
+		        dto.setBooking(GetBookingDTO.fromEntity(payment.getBooking()));
+		        return dto;
+		    }
+
+		public Integer getPaymentId() {
+			return paymentId;
+		}
+
+
+		public void setPaymentId(Integer paymentId) {
+			this.paymentId = paymentId;
+		}
+
+
+		public LocalDate getPaymentDate() {
+			return paymentDate;
+		}
+
+
+		public void setPaymentDate(LocalDate paymentDate) {
+			this.paymentDate = paymentDate;
+		}
+
+
+		public Integer getPaymentAmount() {
+			return paymentAmount;
+		}
+
+
+		public void setPaymentAmount(Integer paymentAmount) {
+			this.paymentAmount = paymentAmount;
+		}
+
+
+		public String getPaymentMethod() {
+			return paymentMethod;
+		}
+
+
+		public void setPaymentMethod(String paymentMethod) {
+			this.paymentMethod = paymentMethod;
+		}
+
+
+		public String getPaymentStatus() {
+			return paymentStatus;
+		}
+
+
+		public void setPaymentStatus(String paymentStatus) {
+			this.paymentStatus = paymentStatus;
+		}
+
+
+		public GetBookingDTO getBooking() {
+			return booking;
+		}
+
+
+		public void setBooking(GetBookingDTO booking) {
+			this.booking = booking;
+		}
+		 
+		 
+	}
 
 }
